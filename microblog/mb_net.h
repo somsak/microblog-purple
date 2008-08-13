446df3b33861d8f7d8b4b38bf4dd76f2cb3726f8
somsaks <somsaks@c93ce494-aa52-0410-a5ec-afe58518fd76>
2008-08-13 15:21:27 +0000

    Fix connection closing bug (really?)

--- microblog/mb_net.h
+++ microblog/mb_net.h
@@ -12,14 +12,23 @@
 extern "C" {
 #endif
 
+#define TwitterAccount MBAccount //< for the sake of simplicity for now
+
+// if handler return
+// 0 - Everything's ok
+// -1 - Requeue the whole process again
+struct _MbConnData;
+
+typedef gint (*MbHandlerFunc)(struct _MbConnData * , gpointer );
+
 typedef struct _MbConnData {
-	TwitterAccount * ta;
+	MBAccount * ta;
 	gchar * error_message;
 	MbHttpData * request;
 	MbHttpData * response;
 	gint retry;
 	gint max_retry;
-	TwitterHandlerFunc handler;
+	MbHandlerFunc handler;
 	gpointer handler_data;
 	gint action_on_error;
 	PurpleSslConnection * conn_data;
@@ -27,11 +36,12 @@ typedef struct _MbConnData {
 	gint conn_id;
 } MbConnData;
 
-extern MbConnData * mb_conn_data_new(void);
+extern MbConnData * mb_conn_data_new(MBAccount * ta);
+extern void mb_conn_data_free(MbConnData * data);
 extern void mb_conn_process_request(gpointer data);
-extern void mb_conn_post_request(gpointer data, PurpleSslConnection * ssl, PurpleInputCondition cond);
-extern void mb_conn_get_result(gpointer data, PurpleSslConnection * ssl, PurpleInputCondition cond);
-extern void mb_conn_connect_error(PurpleSslConnection *ssl, PurpleSslErrorType errortype, gpointer data);
+extern void mb_conn_post_ssl_request(gpointer data, PurpleSslConnection * ssl, PurpleInputCondition cond);
+extern void mb_conn_get_ssl_result(gpointer data, PurpleSslConnection * ssl, PurpleInputCondition cond);
+extern void mb_conn_connect_ssl_error(PurpleSslConnection *ssl, PurpleSslErrorType errortype, gpointer data);
 
 #ifdef __cplusplus
 }
