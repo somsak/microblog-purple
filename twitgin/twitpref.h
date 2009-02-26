ef8f5c595359f2a965c2701db8653f96c51bb0ab
somsaks <somsaks@c93ce494-aa52-0410-a5ec-afe58518fd76>
2009-02-26 11:24:48 +0000

    add preferences

---
+++ twitgin/twitpref.h
@@ -0,0 +1,10 @@
+/**
+ * Preference constant for Twitgin
+ */
+#ifndef __TWITPREF__
+#define __TWITPREF__
+
+#define TW_PREF_PREFIX "/plugins/core/twitgin"
+#define TW_PREF_REPLY_LINK TW_PREF_PREFIX "/reply_link"
+
+#endif
