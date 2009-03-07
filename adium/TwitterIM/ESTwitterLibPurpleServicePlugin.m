edb5c0beb2acb1595db0ffd88f558563dadc48a6
jsippel <jsippel@c93ce494-aa52-0410-a5ec-afe58518fd76>
2009-03-07 22:32:43 +0000

    removed all duplicate /microblog/ files

--- adium/TwitterIM/ESTwitterLibPurpleServicePlugin.m
+++ adium/TwitterIM/ESTwitterLibPurpleServicePlugin.m
@@ -10,18 +10,19 @@
 
 #import "ESTwitterLibPurpleServicePlugin.h"
 #import "ESTwitterService.h"
+//#import "twitterim.c"
 	
 
 @implementation ESTwitterLibPurpleServicePlugin
 
-//extern BOOL purple_init_plugin(void);
+extern BOOL purple_init_plugin(void);
 
 
 - (void) installPlugin
 {
 	[ESTwitterService registerService];
 	AILog(@"Installing Twitter Plugin");
-	//purple_init_twitterim_plugin();
+	purple_init_twitterim_plugin();
 }
 
 - (void)loadLibpurplePlugin
@@ -32,7 +33,7 @@
 - (void) installLibpurplePlugin
 {
 	AILog(@"Loading Twitter LibPurple Plugin");
-	purple_init_twitterim_plugin();
+	//purple_init_twitterim_plugin();
 }
 
 @end
