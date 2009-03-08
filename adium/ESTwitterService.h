4d03f4a9f0e120543c59da38cab34929b6cc726e
jsippel <jsippel@c93ce494-aa52-0410-a5ec-afe58518fd76>
2009-03-08 00:14:26 +0000

    moving all files up to adium directory

---
+++ adium/ESTwitterService.h
@@ -0,0 +1,13 @@
+//
+//  ESTwitterService.h
+//  TwitterIM
+//
+//  Created by jsippel on 1/7/09.
+//
+
+#import <AdiumLibpurple/PurpleService.h>
+
+@interface ESTwitterService : PurpleService {
+
+}
+@end
