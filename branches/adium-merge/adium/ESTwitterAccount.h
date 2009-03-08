3ba00100c152821f30cbde8b87a9321fb0d9984f
jsippel <jsippel@c93ce494-aa52-0410-a5ec-afe58518fd76>
2009-03-08 00:14:26 +0000

    moving all files up to adium directory

---
+++ branches/adium-merge/adium/ESTwitterAccount.h
@@ -0,0 +1,14 @@
+//
+//  ESTwitterAccount.h
+//  TwitterIM
+//
+//  Created by jsippel on 1/7/09.
+//
+
+#import <Cocoa/Cocoa.h>
+#import <AdiumLibpurple/CBPurpleAccount.h>
+
+@interface ESTwitterAccount : CBPurpleAccount {
+
+}
+@end
---
+++ branches/adium-merge/adium/ESTwitterLibPurpleServicePlugin.h
@@ -0,0 +1,12 @@
+//
+//  ESTwitterLibPurpleServicePlugin.h
+//  TwitterIM
+//
+//  Created by jsippel on 1/7/09.
+//
+
+#import <Adium/AIPlugin.h>
+#import <AdiumLibpurple/AILibpurplePlugin.h>
+
+@interface ESTwitterLibPurpleServicePlugin : AIPlugin <AILibpurplePlugin> {}
+@end
