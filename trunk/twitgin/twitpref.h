0daff9620a379aabc15f16fd4c60e1c3bc841d88
somsaks <somsaks@c93ce494-aa52-0410-a5ec-afe58518fd76>
2010-12-03 16:14:00 +0000

    Merge patch from #221

--- trunk/twitgin/twitpref.h
+++ trunk/twitgin/twitpref.h
@@ -27,8 +27,10 @@
 
 #define TW_PREF_PREFIX "/plugins/core/twitgin"
 #define TW_PREF_REPLY_LINK TW_PREF_PREFIX "/reply_link"
+#define TW_PREF_REPLYALL_LINK TW_PREF_PREFIX "/reply_all_link"
 #define TW_PREF_FAV_LINK TW_PREF_PREFIX "/fav_link"
 #define TW_PREF_RT_LINK TW_PREF_PREFIX "/rt_link"
+#define TW_PREF_ORT_LINK TW_PREF_PREFIX "/ort_link"
 #define TW_PREF_MS_LINK TW_PREF_PREFIX "/ms_link"
 #define TW_PREF_AVATAR_SIZE TW_PREF_PREFIX "/avatar_size"
 
